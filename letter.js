91125e4ac04e6e48d9fca89a82c3e0cbdabdfe7f
Bradley Gamboa <bradleygamboa@gmail.com>
2016-10-11 22:41:00 -0500

    polishing up homework

---
+++ letter.js
@@ -0,0 +1,16 @@
+//makes letter appear either blank or as the letter entered
+exports.letter = letter;
+
+function letter(value) {
+	this.value = value;
+	this.show = false;
+	if (this.value == ' ') 
+		this.show = true;
+}
+
+letter.prototype.printInfo = function() {
+	if (this.show) {
+		return this.value;
+	}
+	return "_ ";
+}
---
+++ main.js
@@ -0,0 +1,46 @@
+var game = require("./game.js");
+var wordCons = require("./word.js");
+var letterCons = require("./letter.js");
+var inquirer = require("inquirer");
+var randomWord = game.randomWord;
+var letterGuessed;
+exports.letter;
+
+var myWord = new wordCons.wordCons(game.randomWord);
+var maxGuesses = 15;
+
+function takeAGuess(){
+	console.log(myWord.toString());
+	//ends game
+	if (myWord.guessesMade.length >= maxGuesses){
+		console.log('You have no more guesses. WOMP WOMP.');
+	return;
+	}
+	inquirer.prompt([{
+		name: 'letter',
+		type: 'text',
+		message: 'Enter a letter:',
+		validate: function(str){
+
+			var regEx = new RegExp("^[a-zA-Z\s]{1,1}$");
+			return regEx.test(str);
+				}
+		//game controls
+		}]).then(function(letterInput){
+				var letter = letterInput.letter; 
+				//checks
+				myWord.findLetter(letter);
+					//winner
+					if(myWord.isComplete()){ 
+					console.log('Yes! It was ' + myWord.toString() + '!');
+					return;
+					}
+					//if games does not end continue to the next guess and recurive call
+				console.log('-------------------\n');
+				console.log('You have ' + (maxGuesses - myWord.guessesMade.length) + ' guesses left.')
+				takeAGuess();
+				}
+  );
+}
+//starts game over
+takeAGuess();
